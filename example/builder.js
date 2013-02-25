6e0f9f98840948ca208387ac0083774592e4dda7
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-02-25 02:40:37 -0800

    swapped to node-sass-wrapper for better options

--- example/builder.js
+++ example/builder.js
@@ -1,15 +1,15 @@
 var Builder = require('component-builder')
   , fs      = require('fs')
-  , mkdir   = require('mkdirp')
   , sass    = require('../');
+
+
 
 var builder = new Builder(__dirname);
 
+sass.style('expanded');
 builder.use(sass);
 
-builder.build(function(err, res){
+builder.build(function (err, res) {
   if (err) throw err;
-  mkdir.sync('build');
-  fs.writeFileSync('build/build.js', res.require + res.js);
-  if (res.css) fs.writeFileSync('build/build.css', res.css);
+  fs.writeFileSync('example/build.css', res.css);
 });
--- index.js
+++ index.js
@@ -1,67 +1,86 @@
-var async = require('async')
-  , debug = require('debug')('component-sass')
+var Batch = require('batch')
   , fs    = require('fs')
   , path  = require('path')
-  , sass  = require('sass');
+  , sass  = require('node-sass-wrapper')
+  , debug = require('debug')('component-sass');
 
 
-var compass    = true
-  , importPath;
+
+/**
+ * Options.
+ */
+
+var options = {
+  compass   : true,
+  style     : 'nested',
+  precision : 3,
+  loadPath  : null,
+  require   : null,
+};
 
 
 /**
  * Replace Sass files with CSS files.
  */
-module.exports = function (builder, options) {
 
-  options = options || {
-    useCompass : compass
-  };
+module.exports = function (builder) {
 
   builder.hook('before styles', function (builder, callback) {
     if (!builder.conf.styles) return callback();
 
-    var files = builder.conf.styles.filter(function (name) {
-      return /\.(sass|scss)/i.test(path.extname(name));
+    var files = builder.conf.styles.filter(sassFilter)
+      , batch = new Batch();
+
+    files.forEach(function (file) {
+      batch.push(function (done) {
+        debug('compiling: %s', file);
+
+        var filePath = builder.path(file)
+          , name     = path.basename(file, path.extname(file)) + '.css';
+
+        sass.compile(filePath, options, function (err, css) {
+          if (err) {
+            debug('error compiling: %s, %s', file, err);
+            return done(err);
+          }
+
+          builder.addFile('styles', name, css);
+          builder.removeFile('styles', file);
+          done();
+        });
+
+      });
     });
 
-    processFiles(builder, files, options, callback);
+    batch.end(callback);
   });
 };
 
 
+/**
+ * Use Compass.
+ */
 
-var processFiles = function (builder, files, options, callback) {
-
-  async.forEach(files, function (file, done) {
-
-    debug('compiling: %s', file);
-
-    var filePath = builder.path(file)
-      , name     = path.basename(file, path.extname(file)) + '.css';
-
-    sass.compile(filePath, options, function (err, css) {
-
-      if (err) {
-        debug('error compiling: %s, %s', file, err);
-        return done(err);
-      }
-
-      builder.addFile('styles', name, css);
-      builder.removeFile('styles', file);
-      done();
-    });
-
-  }, callback);
+module.exports.compass = function (enabled) {
+  options.compass = enabled;
 };
 
 
 /**
- * Toggle using Compass.
+ * Set output style.
  */
 
-module.exports.compass = function (enabled) {
-  compass = enabled;
+module.exports.style = function (style) {
+  options.style = style;
+};
+
+
+/**
+ * Set decimal precision.
+ */
+
+module.exports.precision = function (precision) {
+  options.precision = precision;
 };
 
 
@@ -70,6 +89,25 @@ module.exports.compass = function (enabled) {
  * findable by Sass's @import directive.
  */
 
-module.exports.import = function (path) {
-  importPath = path;
+module.exports.loadPath = function (path) {
+  options.loadPath = path;
 };
+
+
+/**
+ * Require Sass plugin.
+ */
+
+module.exports.require = function (plugin) {
+  options.require = plugin;
+};
+
+
+/**
+ * Filtering function for .sass and .scss files.
+ */
+
+function sassFilter (filename) {
+  var ext = path.extname(filename);
+  if (ext === '.sass' || ext === '.scss') return true;
+}
